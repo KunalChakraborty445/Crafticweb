8c6f7113993632d303b68f36a02c4cc359994f42
Kunal <kunalchakr32@gmail.com>
2026-04-11 15:56:26 +0530

    implemented auth model, controller and routes

---
+++ Backend/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { googleAuth, logoutUser } from '../controllers/auth.controller.js';
+
+
+const authRouter = express.Router();
+
+
+authRouter.post('/register', googleAuth);
+
+authRouter.get('/logout', logoutUser)
+
+
+
+export default authRouter
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 import { config } from 'dotenv';
 import express from 'express';
 import dbConnect from './config/db.js';
+import authRouter from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
 const app = express();
@@ -8,9 +11,19 @@ config();
 dbConnect();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
 app.get('/',(req,res)=>{
     res.send("api working fine");
 })
+
+
+app.use('/api/v1/auth', authRouter)
 
 app.listen(PORT,()=>{
     console.log("server running at:",PORT);
