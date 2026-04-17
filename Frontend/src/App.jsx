412b02bf71d1d71f80511f33eb5f1728956a5867
Kunal <kunalchakr32@gmail.com>
2026-04-17 21:47:24 +0530

    refactoe the authentication system and also  65% progress done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
+import UseGetCurrentUser from './hooks/UseGetCurrentUser';
 export const serverUrl = "http://localhost:4000";
 
 const App = () => {
 
+    UseGetCurrentUser();
 
 
   return (
