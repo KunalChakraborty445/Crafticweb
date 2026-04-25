2da2e720c7d3b7cb7146b39e07175ba0d99c4b9f
Kunal <kunalchakr32@gmail.com>
2026-04-25 21:46:07 +0530

    refactor the prrvious code but still lots of work left

--- Frontend/src/redux/userSlice.js
+++ Frontend/src/redux/userSlice.js
@@ -5,16 +5,20 @@ import { createSlice } from "@reduxjs/toolkit";
 const useSlice = createSlice({
     name: "user",
     initialState: {
-        userData: null
+        userData: null,
+         loading: true,
     },
     reducers:{
         setUserData: (state, action)=>{
             state.userData = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
         }
     }
 })
 
 
-export const { setUserData } = useSlice.actions;
+export const { setUserData, setLoading } = useSlice.actions;
 
 export default useSlice.reducer;
