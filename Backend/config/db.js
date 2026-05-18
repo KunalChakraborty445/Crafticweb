106d025e3358371cf0bfc067228d5c2b0ea4d924
Kunal <kunalchakr32@gmail.com>
2026-05-18 23:27:35 +0530

    mongosb changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
-
- const dbConnect = async () => {
-    mongoose.connection.on("connected", ()=> console.log("database connected"));
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/crafticweb`);
-}
+const dbConnect = async () => {
+    try {
+        mongoose.connection.on("connected", () => console.log("database connected"));
+        await mongoose.connect(`${process.env.MONGODB_URI}/crafticweb`, {
+            tls: true,
+        });
+    } catch (err) {
+        console.error("MongoDB connection error:", err.message);
+        process.exit(1);
+    }
+};
 
 export default dbConnect;
