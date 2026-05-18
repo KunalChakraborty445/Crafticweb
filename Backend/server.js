fa6df07597e910e73e7cfb51e614ba595490fca9
Kunal <kunalchakr32@gmail.com>
2026-05-18 23:10:13 +0530

    remove headers

--- Backend/server.js
+++ Backend/server.js
@@ -15,8 +15,6 @@ const app = express();
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.post('/api/v1/stripe/webhook', express.raw({ type: 'application/json' }), stripeWebhookController);
